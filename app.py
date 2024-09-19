27a67e78c633f0048c591f9190961344e0a9cf68
Carlo Sonday <angelosonday@gmail.com>
2024-09-19 11:22:10 +0800

    clear app.py

--- app.py
+++ app.py
@@ -16,39 +16,7 @@ app.layout = html.Div(
     [
         html.Div(
             'Hello World',
-            style={'border': '2px solid green'},
-            className='w-100 text-center'
         ),
-        html.P("My first html.P()", className='placeholder-text'),
-        html.P("My second html.P()", className='placeholder-text'),
-
-        html.Div(
-            "New div here",
-            className='border p-2, m-4'
-        ), 
-        
-        dbc.Row(
-            [
-                dbc.Col('R1C1, width = 8', width=8, className='border'),
-                dbc.Col('R1C2, width = 4', width=4, className='border'),
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col('R2C1, width = 2', width=2, className='border'),
-                dbc.Col('R2C2, width = 2', width=2, className='border'),
-            ]
-        ),
-        
-        
-        html.Br(),
-        html.Div(
-            [
-                html.Div("box1", className='w-25 border m-auto text-center'),
-                html.Div("box2", className='w-25 border m-auto text-center'),
-            ], 
-            className='d-flex justify-content-between'
-        )
     ]
 )
 
